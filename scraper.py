f7007c40dfad915de523e03e27daf607eeb60cf9
Subin Kim <subinlk@uci.edu>
2024-02-17 00:31:14 -0800

    finishing touches for scraper

--- scraper.py
+++ scraper.py
@@ -65,11 +65,8 @@ def extract_next_links(url, resp):
 
             # Detect and avoid crawling very large files, especially if they have low information value
 
-            # parse the content of the page
-            print("Parsing the content of the page...")
             content = resp.raw_response.content
             soup = BeautifulSoup(content, "html.parser")
-            # filter out tags for "Good Content" of the page - removes hyperlinks, navbars, footers, etc.
             # filter out tags for "Good Content" of the page - removes hyperlinks, navbars, footers, etc.
             filtered_tags = soup.find_all(lambda tag: tag.name in ['title', 'p', 'h1', 'h2', 'h3', 'ul', 'ol', 'li', 'table', 'tr', 'td'] and not any(parent.name in ['nav', 'header', 'footer'] for parent in tag.parents))
 
@@ -86,24 +83,14 @@ def extract_next_links(url, resp):
                     file.write(token + ", ")
                 file.write("\n")
 
-            # # if it doesn't meet a certain amount of words, consider it as low info content 
-            # if len(tokens) < 30:
-            #     print("The page has less than 30 words.")
-            #     return links
-
-            # # when we check for similarity, do we wanna check every single link that we have visited? 
-            # # Check similarity with already visited pages to avoid crawling the similar page multiple times
-            # for visited_url, visited_text in visited.items():
-            #     # Compares the current page with the visited pages and if it is similar, it returns an empty list
-            #     # only compare if the pages are similar if they are in the same domain in terms of relative path 
-            #     # DO WE CHECK THE "LINK" or the "URL"? 
-            #     # put the print result in a text file 
-            #     with open("similarity.txt", "a") as file:
-            #         file.write("Comparing " + url + " and " + visited_url + "...\n")
-            #     if url.rsplit('/', 1)[0] + "/" == visited_url.rsplit('/', 1)[0] + "/" and is_similar_fingerprint(text, visited_text):
-            #         print("The page is similar to a page already visited.")
-            #         # DO WE EXIT THE FUNCTION AND GO TO THE NEXT URL IF THE PAGE IS SIMILAR TO A PAGE ALREADY VISITED?
-            #         return links
+            # Check if visited dictionary is not empty
+            if visited:
+                # Get the last key of the dictionary
+                last_key = list(visited.keys())[-1]
+                if url.rsplit('/', 1)[0] + "/" == last_key.rsplit('/', 1)[0] + "/" and is_similar_fingerprint(text, visited[last_key]):
+                    return links
+            else:
+                print("Visited dictionary is empty, cannot compare with the last visited page.")
 
             # count occurrences of words in the content
             words = [word.lower() for word in tokenize(text) if word.lower() not in stop_words]
@@ -126,16 +113,10 @@ def extract_next_links(url, resp):
                 longest_page['word_count'] = current_page_length
 
             # count the relative path of the URL for trap detection 
-            # IF THE LENGTH OF THE SPLIT IS LESS THAN 2, WE THEN DOWNLOAD IT.........
             relative = url.rsplit('/', 1)[0] + "/"
             relative_count[relative] = relative_count.get(relative, 0) + 1
-            if relative_count[relative] > 15: 
+            if relative_count[relative] > 30: 
                 return links 
-
-            # print the link and the relative path count in a text file 
-            with open("relative_count.txt", "a") as file:
-                file.write("Relative URL: " + relative + "\n")
-                file.write("Relative Path Count: " + str(relative_count[relative]) + "\n")
 
             # crawling/scrapping - find all the hyperlinks in the page
             for tag in soup.find_all('a', href=True):
@@ -155,16 +136,12 @@ def extract_next_links(url, resp):
                     # defragment the URL only if the fragment exists and add it to the unique URLs list
                     if "#" in link:
                         link = link.split("#")[0]
-                        # # check if the path count exceeds a threshold
-                        # if relative_count[relative] <= 5:
                         unique_pages.add(link)
                         links.append(link)
                         visited[link] = text
 
                     # if the page doesn't have a fragment, you still add it to the unique URLs list
                     else:
-                        # # check if the path count exceeds a threshold
-                        # if relative_count[relative] <= 5:
                         unique_pages.add(link)
                         links.append(link)
                         visited[link] = text
@@ -190,10 +167,6 @@ def extract_next_links(url, resp):
             file.write("Longest Page: " + longest_page['url'] + "\n")
             file.write("Word Count: " + str(longest_page['word_count']))
 
-        # write common_words to a txt file
-        with open("common_words.txt", "w") as file:
-            for word, count in common_words.items():
-                file.write(word + ": " + str(count) + "\n")
         # write most common words to a text file 
         with open("most_common_words.txt", "w") as file:
             for word, count in sorted(common_words.items(), key=lambda x: x[1], reverse=True)[:50]:
@@ -230,6 +203,9 @@ def is_valid(url):
         if "action=download" in parsed.query:
             return False
 
+       if "action=upload" in parsed.query: 
+            return False 
+
         if "ical=1" in parsed.query:
             return False 
             
@@ -260,18 +236,12 @@ def is_valid(url):
 def tokenize(text):
     # lowercase all words to be independent of capitalization
     text = text.lower()
-    # use regex to find all the words that are a "token"
     # exclude periods at the end of sentences but include them in abbreviations
     tokens = re.findall(r"(?:\w+\.\w+|\w+['’]\w+|\w+)", text)
     # return the list of "tokens" that are found from the text
     return tokens
 
-# implement a fingerprint function for similarity between two pages
-# document is first parsed into words
-# the words are grouped into contiguous n-grams for some n like 3-grams
-# some of the n-grams are selected to represent the document
-# the selected n-grams are hashed to improve literal efficiency
-# the hash values are stored, and documents are compared using the overlap of fingerprints
+# fingerprint function for similarity between two pages
 def is_similar_fingerprint(page1_text, page2_text, n=3, threshold=15):
     # tokenize the text
     page1_tokens = tokenize(page1_text)
@@ -303,67 +273,5 @@ def is_similar_fingerprint(page1_text, page2_text, n=3, threshold=15):
     common_hashes = selected_hashes_page1.intersection(selected_hashes_page2)
     similarity = len(common_hashes) / len(selected_hashes_page1.union(selected_hashes_page2)) * 100
 
-    # write it into similarity.txt here too 
-    with open("similarity.txt", "a") as file:
-        file.write(f"Similarity: {similarity}%\n")
-
     # return true if the similarity is greater than the threshold
     return similarity >= threshold
-
-# # checks if two pages are similar
-# def is_similar(page1_text, page2_text): 
-#     page1_tokens = set(tokenize(page1_text))
-#     page2_tokens = set(tokenize(page2_text))
-
-#     common_tokens = page1_tokens.intersection(page2_tokens)
-#     union_tokens = page1_tokens.union(page2_tokens)
-#     similarity = (len(common_tokens) / len(union_tokens)) * 100
-    
-#     print(f"Similarity: {similarity}%")
-    
-#     return similarity <= 90
- 
-# main function to test 
-if __name__ == "__main__":
-    # url = "https://wics.ics.uci.edu/events/2022-05-22"
-    # url = "https://wics.ics.uci.edu/events/2022-05-23/?ical=1"
-    url = "https://wics.ics.uci.edu/category/news/page/26"
-    
-    parsed = urlparse(url)
-    print(parsed)
-    relative = url.rsplit('/', 1)[0] + "/"
-    # up until the last part of the path
-    # relative = parsed.scheme + "://" + parsed.netloc + parsed.path[]
-    print(relative)
-#     url = "http://www.stat.uci.edu/wp-content/uploads/JuliaPalaciosAbstract6-6-19"
-#     print(urlparse(url))
-    # page1 = "https://www.stat.uci.edu/professors-nan-and-gillen-receive-1-8m-grant-to-study-statistical-methods-for-alzheimers-research"
-    # page2 = "https://www.stat.uci.edu/uci-in-top-10-on-fortunes-list-of-best-masters-in-data-science-programs"
-
-    # print(page1.rsplit('/', 1)[0] + "/")
-    # print(page2.rsplit('/', 1)[0] + "/")
-
-    # url = "http://sli.ics.uci.edu/Classes/2016S-274b?action=download&upname=HW4c.zip"
-    # print(urlparse(url))
-    # if "action=download" in urlparse(url).query:
-    #     print("DOWNLOAD")
-
-    # wics_url = "https://wics.ics.uci.edu/events/2022-02-20"
-    # # get https://wics.ics.uci.edu/events/ from the url 
-    # print(urlparse(wics_url).path)
-    # print(wics_url.rsplit('/', 1)[0] + "/")
-    # google_url = "https://www.google.com/calendar/event?action=TEMPLATE&text=Fall+2022+Week+4%3A+Committee+Applications+Panel&dates=20221019T180000/20221019T190000&details=RSVP%3A+https%3A%2F%2Fbit.ly%2FWICSxCommitteeAppsPanel%C2%A0+%0AWanna+join+our+team%3F+WICS+Committee+applications+are+now+open%2C+and+we%E2%80%99re+here+to+answer+any+questions+you+may+have%2C+from+how+often+we+meet+to+what+we+love+about+being+on+committee.+Join+us+as+our+panelists+from+each+committee+share+with+you+their+experiences+and+all+of+the+reasons+why+you+should+apply%21+Snacks+will+be+provided%2C+so+be+sure+to+RSVP.+Note+that+everyone+is+welcome%2C+regardless+of+gender+identity.+See+you+there%21+%0A%0A%0A%09Related%0A&location=ICS+428&trp=false&sprop=website:https://wics.ics.uci.edu&ctz=America%2FLos_Angeles"
-    # # https://wics.ics.uci.edu/events/ 15
-    # path = urlparse(google_url).path
-    # print(path)
-    # print(urlparse(google_url).netloc)
-    # print(path.split('/')[-1])
-
-    # # given "https://wics.ics.uci.edu/events/2022-02-20", get "https://wics.ics.uci.edu/events/"
-    # # print(url.rsplit('/', 1)[0] + "/")
-
-    # url1 = "https://wics.ics.uci.edu/events/2021-10-11/"
-    # url2 = "https://wics.ics.uci.edu/events/2021-10-10/"
-    # # how do you test if the resp content that is associated with the url is not here? 
-    # # print(is_similar(get_page(url1), get_page(url2)))
-
